970a4f781ca95eac4c2aab1fb777770e06a0be84
kgriem <kgriem@rams.colostate.edu>
2020-11-03 20:13:46 -0700

    everything works now

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 cd ./out/artifacts/Server_jar/
 docker build --tag server3k .
 docker rm server --force
-docker run -it --name=server --network=host server3k &
+docker run -it --name=server -p 3000:3000 server3k
