65fc1b07d3dcf0f4b88ad27f58fd14b80c1162fd
kgriem <kgriem@rams.colostate.edu>
2020-12-07 17:25:02 -0700

    stuff

---
+++ Server/src/DocumentRequest.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class DocumentRequest extends RequestData{
+    private String word;
+    private ArrayList<String> lines;
+    private Boolean valid;
+
+    @Override
+    public void buildResponse() {
+
+    }
+}
